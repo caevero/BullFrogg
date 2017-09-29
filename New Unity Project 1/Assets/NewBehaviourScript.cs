0b5c66e330fdf356c427bed1717d18eeda1689d4
carlitosv11 <verdugorojascarlos@gmail.com>
2017-09-29 11:19:59 -0500

    dfghfgh
    holi

---
+++ New Unity Project 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_ejemplos : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    //VARIABLE INPUT: Sirve como sistema de entrada en los videojuegos, como dices vos: si un videojuego no tiene esto, es una pelicula. Ejemplo: presionar un boton, el mouse, mando, etc.
+
+
+    //EJEMPLO 1:informa de la posición del ratón --> Input.mousePosition
+       public GameObject elemento;
+        void Update()
+    {
+        if (Input.GetButtonDown("Objetico"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray))
+                Instantiate(elemento, transform.position, transform.rotation);
+        }
+
+        //EJEMPLO2: Cada entrada representa el estado de un dedo tocando la pantalla. --> Touch
+
+        int toques = 0;
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+               toques++;
+        }
+
+        if (toques > 0)
+            print("El usuario tiene " + toques + " toques en la pantalla");
+
+        //EJEMPLO 3  Devuelve si se mantiene pulsado el botón del ratón. --> GetMouseButton
+        //El valor del botón izquierdo es 0, 1 para el botón derecho, 2 para el botón central.
+
+        if (Input.GetMouseButton(0))
+            Debug.Log("Presionaste click derecho.");
+
+        if (Input.GetMouseButton(1))
+            Debug.Log("Presionaste click izquierdo.");
+
+        if (Input.GetMouseButton(2))
+            Debug.Log("Presionaste click medio.");
+
+
+        //EJEMPLO 4 Devuelve la medición de la aceleración específica que se produjo durante el último frame --> GetAccelerationEvent
+        Vector3 aceleracion = Vector3.zero;
+        int i = 0;
+        while (i < Input.accelerationEventCount) {
+            AccelerationEvent accEvent = Input.GetAccelerationEvent(i);
+            aceleracion += accEvent.acceleration * accEvent.deltaTime;
+            ++i;
+        }
+        print(aceleracion);
+
+
+        //EJEMPLO 5 Detectar un click o prcuando presiona un botono--> AnyKey
+        if (Input.anyKey)
+            Debug.Log("Se ha detectado un click");
+    }
+
+
+}
+
+
+
+
+
+
+
---
+++ New Unity Project 1/Assets/Timeejemplo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeejemplo : MonoBehaviour {
+
+	
+	void Start () {	
+	}
+
+
+    void Update()
+    {
+        //VARIABLE TIME: Se obtiene información de tiempo de Unity 
+
+
+
+        //EJEMPLO 1: El tiempo en segundos que tardó en completar el último fotograma --> deltaTime
+        float translation = Time.deltaTime * 10;
+        transform.Translate(0, 0, translation);
+    }
+
+        //EJEMPLO  2: Número total de fotogramas que han pasado --> frameCount
+        private static int lastRecalculation = -1;
+         static void RecalculateValue()
+    {
+        if (lastRecalculation == Time.frameCount)
+            return;
+
+        //EJEMPLO 3: La escala a la que pasa el tiempo. Esto se puede utilizar para efectos de camara lenta o camara rapida -->timeScale
+
+        if (Input.GetButtonDown("cosa"))
+        {
+            if (Time.timeScale == 1.0F)
+                Time.timeScale = 0.7F;
+            else
+                Time.timeScale = 1.0F;
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+        }
+
+
+        //EJEMPLO 4: guarda las capturas de pantalla entre cuadros --> captureFramerate 
+        Public int frameRate = 25;
+        Time.captureFramerate = frameRate;
+
+        //EJEMPLO 5:  Lectura de tiempo real en segundos desde que comenzó el juego -->  realtimeSinceStartup
+        print(Time.realtimeSinceStartup);
+    }
+}
